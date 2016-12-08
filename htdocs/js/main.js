f4dea151731f425af3867258a3d095f8f2c6e636
Simon Thompson <simonthompson@hotmail.ca>
2016-12-07 19:36:08 -0500

    Add files via upload

---
+++ htdocs/js/main.js
@@ -0,0 +1,65 @@
+function loadInnerHTML(button){
+	$.ajax({
+    type: 'HEAD',
+    url: 'src/'+button.id+'.php',
+	success: function() {
+		$("#loadContent").load('src/'+button.id + ".php?Id=2"); 
+	},
+	error: function() {
+		$.ajax({
+	    type: 'HEAD',
+	    url: 'src/'+button.id+'.html',
+		success: function() {
+			$("#loadContent").load('src/'+button.id + ".html"); 
+		},
+		error: function() {
+			$("#loadContent").load("src/error.html");
+		}
+		});
+	}
+	});
+}
+
+function loadAttack(id){
+	$.ajax({
+    type: 'HEAD',
+    url: 'src/attack.php',
+	success: function() {
+		$("#loadContent").load("src/attack.php?Id="+id); 
+	},
+	error: function() {
+		alert("error");
+	}
+	});
+}
+
+function redirectHome(playerId, oppId){
+	if(playerId != -1){
+		$.ajax({
+		type: "post",
+		data: {"playerId": playerId, "oppId": oppId},
+		url: 'src/level.php',
+		success: function(response) {
+		},
+		error: function(response) {
+			alert(response);
+		  }
+		});
+	}
+	
+	$.ajax({
+    type: 'HEAD',
+    url: 'src/home.php',
+	success: function() {
+		$("#loadContent").load("src/home.php"); 
+	},
+	error: function() {
+		alert("error");
+	}
+	});
+}
+
+/*For JQuery methods */
+$(document).ready(function() {
+
+});
